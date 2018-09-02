0cf6df6a9ecffa346cbdac4a5819d6919b334dd1
Sagar Mangulkar <smangul1@binghamton.edu>
2018-09-01 23:39:33 -0400

    Add view and create pages, buttons and styles.css with natigation

--- src/main/java/com/basis/WebApp/HelloWorld.java
+++ src/main/java/com/basis/WebApp/HelloWorld.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
 
-    @RequestMapping("/")
-    public String test() {
-        return "Hello World...!";
-    }
+//    @RequestMapping("/")
+//    public String index() {
+//        return "index.html";
+//    }
+//
+//    @RequestMapping("/ViewIncidents.html")
+//    public String incident() {
+//        return "Inside incident...!";
+//    }
+//
+//    @RequestMapping("/ViewSessions.html")
+//    public String session() {
+//        return "Inside session...!";
+//    }
 }
